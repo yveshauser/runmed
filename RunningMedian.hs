dfecd3c281920d71077f4636bbbd6070baedad6d
Yves Hauser <yves.hauser@gmail.com>
2012-02-19 21:16:45 +0100

    code formatting

--- RunningMedian.hs
+++ RunningMedian.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE ImplicitParams #-}
 
 -- | Implementation of a running median smoother according to the
---   algorithm described in Haerdle und Steiger (1995).
+-- algorithm described in Haerdle und Steiger (1995).
 module RunningMedian (
-	  runmed
+          runmed
         , begin_rule
         , end_rule
-	)
+        )
 where
 
 import Prelude hiding ( init, head )
@@ -29,37 +29,37 @@ runmed :: Int       -- ^ The size @k@, where @2*k+1@ is the window size
 runmed k xs = let ?ctx = buildCtx k in runmed' xs
 
 runmed' :: (?ctx :: Ctx) => [Double] -> [Double]
-runmed' xs 
+runmed' xs
   | length xs < window_size = xs
   | otherwise = let k = heap_size in begin_rule k xs ++ runmed'' xs ++ end_rule k xs
 
 runmed'' :: (?ctx :: Ctx) => [Double] -> [Double]
-runmed'' l = let s  = window_size 
-	         l' = zip xs os
-	         xs = drop s l
-	  	 os = map (flip mod $ s) [0..] in 
-              runST $ do h <- build l 
-	                 let ?ind = h 
-	                 init l 
+runmed'' l = let s  = window_size
+                 l' = zip xs os
+                 xs = drop s l
+                 os = map (flip mod $ s) [0..] in
+              runST $ do h <- build l
+                         let ?ind = h
+                         init l
                          liftM2 (:) take_median $ mapM (\(x,o) -> step x o) l'
 
 begin_rule :: Int -> [Double] -> [Double]
-begin_rule = take 
+begin_rule = take
 
 end_rule :: Int -> [Double] -> [Double]
-end_rule k l = let n = (length l) - k in drop n l 
+end_rule k l = let n = (length l) - k in drop n l
 
 step :: (?ind :: Indexed s, ?ctx :: Ctx) => Double -> Int -> ST s Double
-step x_in o = do i <- read_idx_into_elems (o+1) 
+step x_in o = do i <- read_idx_into_elems (o+1)
                  x_out <- read_elem i
                  med <- read_elem idx_median
-                 write_elem i x_in 
+                 write_elem i x_in
                  rebuild_heap i x_out x_in med
-                 take_median 
+                 take_median
 
 rebuild_heap :: (?ind :: Indexed s, ?ctx :: Ctx) => Int -> Double -> Double -> Double -> ST s ()
-rebuild_heap i x_out x_in med 
-        -- min out 
+rebuild_heap i x_out x_in med
+        -- min out
         | x_out > med && x_in >= med = min_out_min_in i
         | x_out > med && x_in <  med = min_out_max_in i
         -- max out
@@ -69,33 +69,33 @@ rebuild_heap i x_out x_in med
         | x_out == med && x_in >  med = med_out_min_in i
         | x_out == med && x_in <  med = med_out_max_in i
         | x_out == med && x_in == med = med_out_med_in i
-  where 
-	max_out_max_in i = move_up_max i >>= max_heapify
-  	min_out_min_in i = move_up_min i >>= min_heapify
-	max_out_min_in i = push_to_max_root i >> swap idx_maxheap_root idx_median >> 
-			   do min_root <- read_elem idx_minheap_root 
-	                      if min_root < x_in then swap idx_median idx_minheap_root >> min_heapify idx_minheap_root
-			      else return ()
-	min_out_max_in i = push_to_min_root i >> swap idx_minheap_root idx_median >> 
-			   do max_root <- read_elem idx_maxheap_root
-	                      if max_root > x_in then swap idx_median idx_maxheap_root >> max_heapify idx_maxheap_root
-			      else return ()
-	med_out_min_in i = do min_root <- read_elem idx_minheap_root 
-	                      if min_root < x_in then swap i idx_median >> swap idx_median idx_minheap_root >> min_heapify idx_minheap_root 
-			      else return ()
-	med_out_max_in i = do max_root <- read_elem idx_maxheap_root 
-	                      if max_root > x_in then swap i idx_median >> swap idx_median idx_maxheap_root >> max_heapify idx_maxheap_root 
-			      else return ()
-	med_out_med_in _ = return ()
+  where
+        max_out_max_in i = move_up_max i >>= max_heapify
+        min_out_min_in i = move_up_min i >>= min_heapify
+        max_out_min_in i = push_to_max_root i >> swap idx_maxheap_root idx_median >>
+                           do min_root <- read_elem idx_minheap_root
+                              if min_root < x_in then swap idx_median idx_minheap_root >> min_heapify idx_minheap_root
+                              else return ()
+        min_out_max_in i = push_to_min_root i >> swap idx_minheap_root idx_median >>
+                           do max_root <- read_elem idx_maxheap_root
+                              if max_root > x_in then swap idx_median idx_maxheap_root >> max_heapify idx_maxheap_root
+                              else return ()
+        med_out_min_in i = do min_root <- read_elem idx_minheap_root
+                              if min_root < x_in then swap i idx_median >> swap idx_median idx_minheap_root >> min_heapify idx_minheap_root
+                              else return ()
+        med_out_max_in i = do max_root <- read_elem idx_maxheap_root
+                              if max_root > x_in then swap i idx_median >> swap idx_median idx_maxheap_root >> max_heapify idx_maxheap_root
+                              else return ()
+        med_out_med_in _ = return ()
 
 
 -- Data structure
 
 data Indexed s = Indexed {
- 	    elems :: STUArray s Int Double
- 	  , idx_into_elems :: STUArray s Int Int
- 	  , idx_into_window :: STUArray s Int Int
- 	} 
+            elems :: STUArray s Int Double
+          , idx_into_elems :: STUArray s Int Int
+          , idx_into_window :: STUArray s Int Int
+        }
 
 read_elem :: (?ind :: Indexed s) => Int -> ST s Double
 read_elem = readArray (elems ?ind)
@@ -120,19 +120,19 @@ swap :: (?ind :: Indexed s) => Int -> Int -> ST s ()
 swap i j = do -- read values
               heap_elem_i <- read_elem i
               heap_elem_j <- read_elem j
-	      win_elem_i <- read_idx_into_window i
-	      win_elem_j <- read_idx_into_window j
-	      pos_elem_k <- read_idx_into_elems win_elem_i
-	      pos_elem_l <- read_idx_into_elems win_elem_j
-	      -- update heap
-              write_elem j heap_elem_i 
-              write_elem i heap_elem_j 
-	      -- update position index
-	      write_idx_into_elems win_elem_i pos_elem_l
-	      write_idx_into_elems win_elem_j pos_elem_k
-	      -- update window index
-	      write_idx_into_window j win_elem_i
-	      write_idx_into_window i win_elem_j
+              win_elem_i <- read_idx_into_window i
+              win_elem_j <- read_idx_into_window j
+              pos_elem_k <- read_idx_into_elems win_elem_i
+              pos_elem_l <- read_idx_into_elems win_elem_j
+              -- update heap
+              write_elem j heap_elem_i
+              write_elem i heap_elem_j
+              -- update position index
+              write_idx_into_elems win_elem_i pos_elem_l
+              write_idx_into_elems win_elem_j pos_elem_k
+              -- update window index
+              write_idx_into_window j win_elem_i
+              write_idx_into_window i win_elem_j
 
 {-# INLINE left #-}
 left :: Int -> Int
@@ -166,69 +166,69 @@ min_heapify :: (?ind :: Indexed s, ?ctx :: Ctx) => Int -> ST s ()
 min_heapify = heapify Min heap_size
 
 heapify :: (?ind :: Indexed s, ?ctx :: Ctx) => Prio -> Int -> Int -> ST s ()
-heapify p s i = do l <- heapify_l p s i 
-                   m <- heapify_r p s i l 
-	  	   if m == i then return ()
-		   else swap i m >> heapify p s m
+heapify p s i = do l <- heapify_l p s i
+                   m <- heapify_r p s i l
+                   if m == i then return ()
+                   else swap i m >> heapify p s m
 
 heapify_l :: (?ind :: Indexed s, ?ctx :: Ctx) => Prio -> Int -> Int -> ST s Int
 heapify_l Min s i = if l > s then return i
-		    else idx_of (<) (l+o) i 
-		where l = left (i-o) 
-		      o = idx_minheap_root-1
+                    else idx_of (<) (l+o) i
+                where l = left (i-o)
+                      o = idx_minheap_root-1
 
 heapify_l Max s i = let l = left i in
-	            if l > s then return i
-		    else idx_of (>) l i 
+                    if l > s then return i
+                    else idx_of (>) l i
 
 heapify_r :: (?ind :: Indexed s, ?ctx :: Ctx) => Prio -> Int -> Int -> Int -> ST s Int
 heapify_r Min s i m = if r > s then return m
-		      else idx_of (<) (r+o) m
-		where r = right (i-o)
-		      o = idx_minheap_root-1
+                      else idx_of (<) (r+o) m
+                where r = right (i-o)
+                      o = idx_minheap_root-1
 
 heapify_r Max s i m = let r = right i in
-		      if r > s then return m
-		      else idx_of (>) r m
+                      if r > s then return m
+                      else idx_of (>) r m
 
 idx_of :: (?ind :: Indexed s) => (Double -> Double -> Bool) -> Int -> Int -> ST s Int
 idx_of r i j = cmp r i j >>= \cond -> if cond then (return i) else (return j)
 
 cmp :: (?ind ::  Indexed s) => (Double -> Double -> Bool) -> Int -> Int -> ST s Bool
-cmp r i j = liftM2 r (read_elem i) (read_elem j) 
+cmp r i j = liftM2 r (read_elem i) (read_elem j)
 
 push_to_max_root :: (?ind :: Indexed s, ?ctx :: Ctx) => Int -> ST s ()
 push_to_max_root = push_to_idx idx_maxheap_root
 
 push_to_min_root :: (?ind :: Indexed s, ?ctx :: Ctx) => Int -> ST s ()
-push_to_min_root = push_to_idx idx_minheap_root 
+push_to_min_root = push_to_idx idx_minheap_root
 
 push_to_idx :: (?ind :: Indexed s) => Int -> Int -> ST s ()
-push_to_idx r i 
-	| i == r = return ()
-	| otherwise = let j = parent_with_offset i r in
-		      swap i j >> push_to_idx r j
+push_to_idx r i
+        | i == r = return ()
+        | otherwise = let j = parent_with_offset i r in
+                      swap i j >> push_to_idx r j
 
 move_up_max :: (?ind :: Indexed s, ?ctx :: Ctx) => Int -> ST s Int
-move_up_max = move_up Max 
+move_up_max = move_up Max
 
 move_up_min :: (?ind :: Indexed s, ?ctx :: Ctx) => Int -> ST s Int
 move_up_min = move_up Min
 
 move_up :: (?ind :: Indexed s, ?ctx :: Ctx) => Prio -> Int -> ST s Int
 move_up Min i = let p = parent_with_offset i o in
-	        if (o > p) then return i
-	        else do cond <- cmp (<) i p 
+                if (o > p) then return i
+                else do cond <- cmp (<) i p
                         if cond then swap i p >> move_up Min p
-		        else return i
-	 where o = idx_minheap_root
+                        else return i
+         where o = idx_minheap_root
 
 move_up Max i = let p = parent i in
-	        if (o > p) then return i
-	        else do cond <- cmp (>) i p 
+                if (o > p) then return i
+                else do cond <- cmp (>) i p
                         if cond then swap i p >> move_up Max p
-		        else return i
-	 where o = idx_maxheap_root
+                        else return i
+         where o = idx_maxheap_root
 
 -- Also having a context as implicit parameter, in order to statically
 -- get the window size and dependent parameters
@@ -265,15 +265,14 @@ take_median = readArray (elems ?ind) idx_median
 
 init :: (?ind :: Indexed s, ?ctx :: Ctx) => [Double] -> ST s ()
 init l = heapsort window_size >> reverse idx_maxheap_root heap_size
-	where reverse i j 
-		| i < j = swap i j >> reverse (succ i) (pred j)
-		| otherwise = return ()
+        where reverse i j
+                | i < j = swap i j >> reverse (succ i) (pred j)
+                | otherwise = return ()
 
 build :: (?ctx :: Ctx) => [Double] -> ST s (Indexed s)
 build l = liftM3 Indexed heap idx_into_heap idx_into_window
-	  where
+          where
              heap = newListArray (1, up_idx) l
-	     idx_into_heap = newListArray (1, up_idx) [1 .. up_idx]
-	     idx_into_window = newListArray (1, up_idx) [1 .. up_idx]
-	     up_idx = window_size
-
+             idx_into_heap = newListArray (1, up_idx) [1 .. up_idx]
+             idx_into_window = newListArray (1, up_idx) [1 .. up_idx]
+             up_idx = window_size
