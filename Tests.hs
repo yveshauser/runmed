1274672083c12e7a45ab25c8f8cffe4726d80b76
Yves Hauser <yves.hauser@gmail.com>
2012-05-09 22:54:36 +0200

    cleanups

--- Tests.hs
+++ Tests.hs
@@ -1,35 +1,6 @@
 import RunningMedian
---import IndexedHeap as I
-
 import Test.QuickCheck
---import Math.Statistics
-
-import Control.Monad
-import Control.Monad.ST
-import Data.Array.ST
-import Data.Array.Unboxed as U
-import Data.List
-
-{-
-build_from_list :: [Double] -> ST s (IndexedHeap s)
-build_from_list l = liftM3 IndexedHeap heap idx_into_heap idx_into_window 
-	   where
-             heap = newListArray (1, up_idx) l
-	     idx_into_heap = newListArray (1, up_idx) [1 .. up_idx]
-	     idx_into_window = newListArray (1, up_idx) [1 .. up_idx]
-	     up_idx = length l
-
-hsort :: [Double] -> [Double]
-hsort [] = []
-hsort l = U.elems $ runSTUArray $ do s <- build_from_list l
-				     let ?heap = s
-                                     heapsort (length l) 
-	                             return $ I.elems ?heap
-
-prop_sort_model xs = sort xs == hsort xs
-qc1 = quickCheck (prop_sort_model :: [Double] -> Bool)
--}
-
+import Math.Statistics
 
 runmed_naive :: Int -> [Double] -> [Double]
 runmed_naive k l 
@@ -40,19 +11,9 @@ moving_median _ [] = []
 moving_median k l@(x:xs) 
   | length l < 2*k+1 = []
   | otherwise = let window = take (2*k+1) l in 
-                median' window : moving_median k xs
+                median window : moving_median k xs
 
 k = 3
 prop_median_model xs = runmed k xs == runmed_naive k xs
-qc2 = quickCheck (prop_median_model :: [Double] -> Bool)
+qc1 = quickCheck (prop_median_model :: [Double] -> Bool)
 
--- copied from hstats (cabal install failed...)
-mean :: Floating a => [a] -> a
-mean x = fst $ foldl' (\(!m, !n) x -> (m+(x-m)/(n+1),n+1)) (0,0) x
-
-median' :: (Floating a, Ord a) => [a] -> a
-median' x | odd n  = head  $ drop (n `div` 2) x'
-         | even n = mean $ take 2 $ drop i x'
-                  where i = (length x' `div` 2) - 1
-                        x' = sort x
-                        n  = length x
